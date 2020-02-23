08bcedfb0f76075e02a525338396add6f0bb3fd7
paulovieiradasilva <vieiraweb.tecnologia@gmail.com>
2020-02-22 22:31:59 -0300

    correcao do redirect de /home para /dashboard

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,7 +70,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @auth
-                        <a href="{{ route('home') }}">Home</a>
+                        <a href="{{ route('home') }}">Dashboard</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
 
