3b894f5e33a78dcd3dca0de94b59628bcb5e8bab
Paulo Vieira da Silva <vieiraweb.tecnologia@gmail.com>
2020-05-23 23:32:04 -0300

    + animacoes edit/delete

--- resources/views/admin/applications/_actions.blade.php
+++ resources/views/admin/applications/_actions.blade.php
@@ -1,6 +1,6 @@
-<a href="javascript:void(0);" onclick="edit({{ $id }})" class="btn btn-xs btn-default">
+<a href="javascript:void(0);" onclick="edit({{ $id }})" class="btn btn-xs btn-default animated zoomIn">
     <i class="fas fa-edit"></i>
 </a>
-<a href="javascript:void(0);" onclick="confirmation({{ $id }})" class="btn btn-danger btn-xs">
+<a href="javascript:void(0);" onclick="confirmation({{ $id }})" class="btn btn-danger btn-xs animated zoomIn">
     <i class="fas fa-trash-alt"></i>
 </a>
